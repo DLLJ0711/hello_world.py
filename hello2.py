44c3737dec030cf9ca885875d266de84a527b4b1
David McFadden <podll0711@gmail.com>
2021-01-12 15:06:47 -0600

    add madlib.py

---
+++ hello2.py
@@ -0,0 +1,6 @@
+name = input("What is you name? ")
+
+
+print("HELLO, " + name.upper())
+nameLength = len(name)
+print("YOUR NAME HAS " + str(nameLength) + " LETTERS IN IT, AWESOME!")
--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,4 @@
 name = input("Who are you? ")
 
 
-print("Hello, " + name)
+print("HELLO, " + name)
