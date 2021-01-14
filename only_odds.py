4dbfb50e7966f6f4c21e0c50ffc305cd94015256
David McFadden <podll0711@gmail.com>
2021-01-14 15:13:33 -0600

    add phonebook_dict.py

---
+++ only_odds.py
@@ -0,0 +1,5 @@
+def only_evens(num % == 0):
+    print(only_evens)
+    return
+
+print(only_evens(2))
---
+++ read_heroes.py
@@ -0,0 +1,3 @@
+with open("heroes.txt', 'r') as file_handle:
+    contents = file_handle.read()
+    print(contents)
