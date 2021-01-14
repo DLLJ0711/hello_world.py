8aa4fb5ef04bf31c800355b14d3a55210d3f49e1
David McFadden <podll0711@gmail.com>
2021-01-14 13:01:43 -0600

    add only_odds.py

--- degree_function.py
+++ degree_function.py
@@ -3,7 +3,8 @@ def C_to_F(Celsius):
     return(Celsius * 9/5) + 32
 print(C_to_F(28))
 
-# C = int(input('What is temperature in C? '))
 
-# F = (C * 9/5) + 32 
-# print(str(F) + ' F')
+def F_to_C(Fahrenheit):
+    return(Celsius * 9/5) + 32
+print(C_to_F(28))
+
---
+++ is_even.py
@@ -0,0 +1,15 @@
+def is_even(num):
+    if num % 2 == 0:
+        print(True)
+    else: print(False)
+
+
+is_even(6)
+
+def is_odd(num):
+    return not is_even(num)
+    
+
+
+is_odd(6)
+
